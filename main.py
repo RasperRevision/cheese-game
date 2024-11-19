a904489abae81b3a8cc1a65f30163ae9283cf5e0
Karl-MrX <20StocksJ@roundwoodpark.co.uk>
2024-11-19 09:18:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pygame
 
+pygame.init()
+screen=pygame.display.set_mode((1280,720))
+clock=pygame.time.Clock()
+run=True
+while run:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run=False
+  key=pygame.key.get_pressed()
+  if key[pygame.K_q]:
+    screen.fill("black")
+  pygame.display.flip()
+  clock.tick(60)
+pygame.quit()
