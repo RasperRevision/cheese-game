365d457e92b14424e219bd8560430a0d127f20d0
RasperRevision <131694670+RasperRevision@users.noreply.github.com>
2024-11-20 10:15:21 +0000

    added tiles

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import pygame
+from tiles import Tileset, Tilemap
 
-pygame.init()
-screen=pygame.display.set_mode((1280,720))
-clock=pygame.time.Clock()
-run=True
-while run:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run=False
-  key=pygame.key.get_pressed()
-  if key[pygame.K_q]:
-    screen.fill("black")
-  pygame.display.flip()
-  clock.tick(60)
-pygame.quit()
+class Game:
+    WIDTH = 1280
+    HEIGHT = 720
+    SIZE = WIDTH, HEIGHT
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(Game.SIZE)
+        pygame.display.set_caption("Cheese Game")
+        self.clock=pygame.time.Clock()
+        
+    
+    def run(self):
+        self.run = True
+        while self.run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
+            key = pygame.key.get_pressed()
+            if key[pygame.K_q]:
+                self.screen.fill("black")
+            pygame.display.flip()
+            self.clock.tick(60)
+        pygame.quit()
+
+
+game = Game()
+
+tileset = Tileset("/assets/tileset/")
+tilemap = Tilemap(tileset, (8, 8), tileset.rect)
+
+game.run()
